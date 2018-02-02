51833aebb230bd1de5e719959c9b25d5d300be5a
dani120k <forolymp120@gmail.com>
2018-02-02 12:53:13 +0300

    Initial commit

---
+++ Application/src/test/java/botbackend/DataBase.java
@@ -0,0 +1,84 @@
+package botbackend;
+import org.telegram.telegrambots.api.objects.Update;
+
+import java.sql.*;
+
+import static java.lang.Boolean.FALSE;
+
+
+public class DataBase {
+    public static Connection conn;
+    public static Statement statmt;
+    public static ResultSet resSet;
+
+    // --------ПОДКЛЮЧЕНИЕ К БАЗЕ ДАННЫХ--------
+    public static void Conn() throws ClassNotFoundException, SQLException
+    {
+        conn = null;
+        Class.forName("org.sqlite.JDBC");
+        conn = DriverManager.getConnection("jdbc:sqlite:TEST1.s3db");
+
+        System.out.println("База Подключена!");
+    }
+
+    // --------Создание таблицы--------
+    public static void CreateDB() throws ClassNotFoundException, SQLException
+    {
+        statmt = conn.createStatement();
+        statmt.execute("CREATE TABLE if not exists 'users' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'name' text, 'tgid' INT);");
+
+        System.out.println("Таблица создана или уже существует.");
+    }
+
+    // --------Заполнение таблицы--------
+    public static void WriteDB(Update update) throws SQLException
+    {
+        try
+        {
+            String name = update.getMessage().getChat().getFirstName();
+            int tgid = update.getMessage().getChatId().intValue();
+            if (name != null)
+            {
+                if (statmt.execute("SELECT 'users' FROM 'tgid' WHERE 'tgid' = tgid LIMIT = 1);") != FALSE) {
+                    PreparedStatement st = conn.prepareStatement("INSERT INTO 'users' ('name', 'tgid') VALUES (?, ?)");
+                    st.setString(1, name);
+                    st.setInt(2, tgid);
+                }
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+
+        System.out.println("Таблица заполнена");
+    }
+
+    // -------- Вывод таблицы--------
+    public static void ReadDB() throws ClassNotFoundException, SQLException
+    {
+        resSet = statmt.executeQuery("SELECT * FROM users");
+
+        while(resSet.next())
+        {
+            int id = resSet.getInt("id");
+            String  name = resSet.getString("name");
+            String  tgid = resSet.getString("tgid");
+            System.out.println( "ID = " + id );
+            System.out.println( "name = " + name );
+            System.out.println( "tgid = " + tgid );
+            System.out.println();
+        }
+
+        System.out.println("Таблица выведена");
+    }
+
+    // --------Закрытие--------
+    public static void CloseDB() throws ClassNotFoundException, SQLException
+    {
+        conn.close();
+        statmt.close();
+        resSet.close();
+
+        System.out.println("Соединения закрыты");
+    }
+
+}
---
+++ Application/src/test/java/botbackend/Example.java
@@ -0,0 +1,120 @@
+package botbackend;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.User;
+import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.generics.LongPollingBot;
+import org.telegram.telegrambots.logging.BotLogger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example extends TelegramLongPollingBot{
+    List<Long> arr = new ArrayList<>(0);
+
+    @Override
+    public String getBotUsername() {
+        return "AntiHeapBot";
+        //возвращаем юзера
+    }
+
+    public void sayHelloWorld(Update update) {
+        if (!update.hasMessage() || !update.getMessage().isUserMessage() || !update.getMessage().hasText() || update.getMessage().getText().isEmpty())
+            return;
+        User maybeAdmin = update.getMessage().getFrom();
+       /* Query DB for if the user is an admin, can be SQL, Reddis, Ignite, etc...
+          If user is not an admin, then return here.
+       */
+
+        SendMessage snd = new SendMessage();
+        snd.setChatId(update.getMessage().getChatId());
+        snd.setText("Привет мир!" + arr.size());
+
+        try {
+            execute(snd);
+        } catch (TelegramApiException e) {
+            BotLogger.error("Could not send message", "waat", e);
+        }
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (arr.indexOf(update.getMessage().getChatId()) == -1)
+            arr.add(update.getMessage().getChatId());
+        System.out.println(update.getMessage().getText());
+        sayHelloWorld(update);
+        /*
+        Message msg = e.getMessage(); // Это нам понадобится
+        String txt = msg.getText();
+        if (txt.equals("/start")) {
+            sendMsg(msg, "Добрый день, я бот который будет присылать вам новости! Для начала пройдите небольшой опрос. По шкале от 1 до 4 в порядке возрастания:");
+
+
+            SendMessage message = new SendMessage();
+            message.setChatId(e.getMessage().getChatId());
+            message.setText("Насколько вам нравится активный вид отдыха?");
+
+            // Create ReplyKeyboardMarkup object
+            ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+            // Create the keyboard (list of keyboard rows)
+            List<KeyboardRow> keyboard = new ArrayList<>();
+            // Create a keyboard row
+            KeyboardRow row = new KeyboardRow();
+            // Set each button, you can also use KeyboardButton objects if you need something else than text
+            row.add("1");
+            row.add("2");
+            row.add("3");
+            row.add("4");
+            // Add the first row to the keyboard
+            keyboard.add(row);
+            // Create another keyboard row
+            // Set the keyboard to the markup
+            keyboardMarkup.setKeyboard(keyboard);
+            // Add it to the message
+            message.setReplyMarkup(keyboardMarkup);
+
+            try {
+                // Send the message
+                execute(message);
+            } catch (TelegramApiException ex) {
+                ex.printStackTrace();
+            }
+        }
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }*/
+    }
+
+    @Override
+    public String getBotToken() {
+        return "531412915:AAGIMCbWWt7kL-AtOTf5IuA4IWOFjrKdnWM";
+        //Токен бота
+    }
+
+    @SuppressWarnings("deprecation") // Означает то, что в новых версиях метод уберут или заменят
+    private void sendMsg(Message msg, String text) {
+        SendMessage s = new SendMessage();
+        s.setChatId(msg.getChatId()); // Боту может писать не один человек, и поэтому чтобы отправить сообщение, грубо говоря нужно узнать куда его отправлять
+        s.setText(text);
+        try { //Чтобы не крашнулась программа при вылете Exception
+            sendMessage(s);
+        } catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+
+}
