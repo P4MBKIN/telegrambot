a8237c8970b6b7f2af5fa3afff310a4e9e375e36
dani120k <foroltmp120@gmail.com>
2018-02-20 02:30:54 +0300

    Update

--- Application/src/test/java/botbackend/Application.java
+++ Application/src/test/java/botbackend/Application.java
@@ -9,14 +9,6 @@ import java.util.ArrayList;
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        //VKNewsRequest tmp = new VKNewsRequest();
-
-        //ArrayList<News> allNews = tmp.getVKNews(VKNames., 100, 19);
-        //System.out.println(allNews.size() + "size");
-        //for(int i = 0; i < allNews.size(); i++){
-         //   allNews.get(i).writeNews("4CH"+ (i+1), "");
-        //}
-
         ApiContextInitializer.init();
         DataBase.Conn();
         DataBase.CreateDB();
@@ -24,9 +16,6 @@ public class Application {
         Bot bot = new Bot();
         try {
             botsApi.registerBot(bot);
-            //bot.UpdateNews(allNews);
-
-
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
--- Application/src/test/java/botbackend/Bot.java
+++ Application/src/test/java/botbackend/Bot.java
@@ -1,96 +1,43 @@
 package botbackend;
 
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Predicate;
-import javafx.beans.binding.ListBinding;
-import javafx.collections.ObservableList;
-import org.jetbrains.annotations.NotNull;
-import org.telegram.abilitybots.api.bot.AbilityBot;
-import org.telegram.abilitybots.*;
-import org.telegram.abilitybots.api.objects.Ability;
-import org.telegram.abilitybots.api.objects.Flag;
-import org.telegram.abilitybots.api.objects.Reply;
-import org.telegram.abilitybots.api.sender.MessageSender;
-import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.methods.send.SendPhoto;
-import org.telegram.telegrambots.api.methods.send.SendVideoNote;
-import org.telegram.telegrambots.api.objects.CallbackQuery;
-import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
-import org.telegram.telegrambots.api.objects.User;
-import org.telegram.telegrambots.api.objects.games.CallbackGame;
-import org.telegram.telegrambots.api.objects.inlinequery.InlineQuery;
-import org.telegram.telegrambots.api.objects.replykeyboard.ForceReplyKeyboard;
 import org.telegram.telegrambots.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
-import org.telegram.telegrambots.exceptions.TelegramApiValidationException;
 import org.telegram.telegrambots.generics.LongPollingBot;
-import org.telegram.telegrambots.logging.BotLogger;
 
 import javax.imageio.ImageIO;
-import javax.xml.crypto.Data;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.nio.Buffer;
 import java.util.*;
-import java.util.function.Consumer;
-
 import static botbackend.VKNames.*;
-import static org.telegram.abilitybots.api.objects.Locality.ALL;
-import static org.telegram.abilitybots.api.objects.Privacy.PUBLIC;
-import static org.telegram.abilitybots.api.util.AbilityUtils.getChatId;
 
-public class Bot extends AbilityBot{
-
+public class Bot extends TelegramLongPollingBot{
     List<String> Question = new ArrayList<>();
-
+    Hashtable<Long, VKNewsRequest> usersChoice = new Hashtable<>(0);
 
     @Override
-    public int creatorId(){
-        return 123213;
+    public String getBotUsername() {
+        return "AntiHeapbot";
     }
 
-    public void UpdateNews(ArrayList<News> news) throws Exception{
-        System.out.println("Inmethod");
-        List<Integer> ids = DataBase.getAllChatId();
-        System.out.println(ids.size());
-        System.out.println("newsize" + news.size());
-        for(int i = 0; i < ids.size(); i++)
-        {
-            for(int j = 0; j < news.size(); j++)
-            {
-                System.out.println(news.size() + "allo");
-                try
-                {
-                    BufferedImage img = news.get(j).getAllNewsPicture();
-                    ByteArrayOutputStream os = new ByteArrayOutputStream();
-                    ImageIO.write(img, "jpg", os);
-                    InputStream is = new ByteArrayInputStream(os.toByteArray());
-
-                    SendPhoto photo = new SendPhoto();
-                    photo.setChatId(ids.get(i).longValue());
-                    photo.setNewPhoto("newnews", is);
-                    photo.setReplyMarkup(createKeyboard(news.get(j).getLinkPost()));
-                    System.out.println(news.get(j).getLinks() + "link");
-                    sendPhoto(photo);
-                }catch (Exception ex){
-                    ex.printStackTrace();
-                }
-            }
-        }
-
+    @Override
+    public String getBotToken() {
+        return "531412915:AAGIMCbWWt7kL-AtOTf5IuA4IWOFjrKdnWM";
     }
 
+    /**
+     * Метод, отправляюший новости по заданному чат id
+     * @param news arraylist с новостями
+     * @param chatId номер чата
+     */
     public void UpdateNewsToChatId(ArrayList<News> news, Long chatId) {
         for (int j = 0; j < news.size(); j++) {
             System.out.println(news.size() + "allo");
@@ -100,11 +47,11 @@ public class Bot extends AbilityBot{
                 ImageIO.write(img, "jpg", os);
                 InputStream is = new ByteArrayInputStream(os.toByteArray());
 
-                SendPhoto photo = new SendPhoto();
-                photo.setChatId(chatId);
-                photo.setNewPhoto("newnews", is);
-                photo.setReplyMarkup(createKeyboard(news.get(j).getLinkPost()));
-                System.out.println(news.get(j).getLinks() + "link");
+                SendPhoto photo = new SendPhoto()
+                        .setChatId(chatId)
+                        .setNewPhoto("newnews", is)
+                        .setReplyMarkup(createKeyboard(news.get(j).getLinkPost()));
+
                 sendPhoto(photo);
             } catch (Exception ex) {
                 ex.printStackTrace();
@@ -112,22 +59,38 @@ public class Bot extends AbilityBot{
         }
     }
 
+    /**
+     * Метод для создания ссылки на новость под фото
+     * @param link
+     * @return
+     */
     InlineKeyboardMarkup createKeyboard(String link){
         InlineKeyboardMarkup mark = new InlineKeyboardMarkup();
 
         List<InlineKeyboardButton> buttons = new ArrayList<>();
-        InlineKeyboardButton buta = new InlineKeyboardButton();
-        buta.setText("Ссылочка");
-        buta.setUrl(link);
-        buttons.add(buta);
+        InlineKeyboardButton but = new InlineKeyboardButton()
+                .setText("Ссылка")
+                .setUrl(link);
+        buttons.add(but);
         List<List<InlineKeyboardButton>> list = new ArrayList<>();
         list.add(buttons);
         mark.setKeyboard(list);
         return mark;
     }
 
+    /**
+     * Конструктор
+     */
     public Bot(){
-        super("531412915:AAGIMCbWWt7kL-AtOTf5IuA4IWOFjrKdnWM", "testbot");
+        VKNewsRequest a = new VKNewsRequest();
+        try {
+            System.out.println(a.getVKNews(VKNames.NAUKA_I_TECHNICA, 100, 4).size());
+            System.out.println(a.getVKNews(VKNames.NAUKA_I_TECHNICA, 100, 4).size());
+            System.out.println(a.getVKNews(VKNames.NAUKA_I_TECHNICA, 100, 4).size());
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        System.out.println("adsgasdgdsagdsg");
         Question.add("Интересуетесь ли вы политикой?");
         Question.add("Нравится ли вам кино?");
         Question.add("Нравится ли вам наука и техника?");
@@ -138,32 +101,47 @@ public class Bot extends AbilityBot{
 
     }
 
-    void setOproc(Long chatId, int ques){
-        SendMessage message = new SendMessage();
+    /**
+     * Метод для создания вопроса
+     * @param ques Номер вопроса
+     * @return
+     */
+    InlineKeyboardMarkup createKeyboardForQues(int ques){
         InlineKeyboardMarkup mark = new InlineKeyboardMarkup();
 
         List<InlineKeyboardButton> buttons = new ArrayList<>();
-        InlineKeyboardButton buta = new InlineKeyboardButton();
-        buta.setText("Нет");
-        buta.setCallbackData("No " + ques);
-        buttons.add(buta);
-        InlineKeyboardButton buta1 = new InlineKeyboardButton();
-        buta1.setText("Возможно");
-        buta1.setCallbackData("Maybe " + ques);
-        buttons.add(buta1);
-        InlineKeyboardButton buta2 = new InlineKeyboardButton();
-        buta2.setText("Да");
-        buta2.setCallbackData("Yes " + ques);
 
-        buta2.setCallbackGame(new CallbackGame());
-        buttons.add(buta2);
+        InlineKeyboardButton but = new InlineKeyboardButton()
+                .setText("Нет")
+                .setCallbackData("No " + ques);
+        buttons.add(but);
+
+        InlineKeyboardButton but1 = new InlineKeyboardButton()
+                .setText("Возможно")
+                .setCallbackData("Maybe " + ques);
+        buttons.add(but1);
+
+        InlineKeyboardButton but2 = new InlineKeyboardButton()
+                .setText("Да")
+                .setCallbackData("Yes " + ques);
+        buttons.add(but2);
 
         List<List<InlineKeyboardButton>> list = new ArrayList<>();
         list.add(buttons);
         mark.setKeyboard(list);
-        message.setReplyMarkup(mark);
-        message.setText(Question.get(ques));
-        message.setChatId(chatId);
+        return mark;
+    }
+
+    /**
+     * Метод для проведения опроса
+     * @param chatId номер чата
+     * @param ques номер вопроса в массиве Question
+     */
+    void setQues(Long chatId, int ques){
+        SendMessage message = new SendMessage()
+                .setReplyMarkup(createKeyboardForQues(ques))
+                .setText(Question.get(ques))
+                .setChatId(chatId);
         try{
             execute(message);
         }catch (Exception ex){
@@ -171,99 +149,103 @@ public class Bot extends AbilityBot{
         }
     }
 
+    /**
+     * Обрабатываем входяший запрос при опросе
+     * @param update
+     */
+    void workWithCallbackQuery(Update update){
+        /*try {
+            DataBase.ReadDB();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }*/
+        int number = Integer.parseInt(update.getCallbackQuery().getData().split(" ")[1]);
+        try {
+            DataBase.setInfoIntoDatabase(update.getCallbackQuery().getMessage().getChatId(), update.getCallbackQuery());
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        if (number < Question.size()-1)
+            setQues(update.getCallbackQuery().getMessage().getChatId(), number+1);
+        else {
+            SendMessage message = new SendMessage();
+            message.setText("Настройки");
+            message.setChatId(update.getCallbackQuery().getMessage().getChatId());
+            ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+            replyKeyboardMarkup.setSelective(true);
+            replyKeyboardMarkup.setResizeKeyboard(true);
+            replyKeyboardMarkup.setOneTimeKeyboard(false);
 
-    @Override
-    public void onUpdateReceived(Update update) {
+            List<KeyboardRow> keyboard = new ArrayList<>();
+            KeyboardRow keyboardFirstRow = new KeyboardRow();
+            keyboardFirstRow.add("Обновить новости");
+            KeyboardRow keyboardSecondRow = new KeyboardRow();
+            keyboardSecondRow.add("Настройки");
+            keyboard.add(keyboardFirstRow);
+            keyboard.add(keyboardSecondRow);
+            replyKeyboardMarkup.setKeyboard(keyboard);
+            message.setReplyMarkup(replyKeyboardMarkup);
 
-        if(update.hasCallbackQuery()){
             try {
-                DataBase.ReadDB();
-            }catch (Exception ex){
-                ex.printStackTrace();
-            }
-            int number = Integer.parseInt(update.getCallbackQuery().getData().split(" ")[1]);
-            try {
-                DataBase.setInfoIntoDatabase(update.getCallbackQuery().getMessage().getChatId(), update.getCallbackQuery());
-            }catch (Exception ex){
-                ex.printStackTrace();
-            }
-            if (number < Question.size()-1)
-                setOproc(update.getCallbackQuery().getMessage().getChatId(), number+1);
-            else
-            {
-
-
-                SendMessage message = new SendMessage();
-                message.setText("Настройки");
-                message.setChatId(update.getCallbackQuery().getMessage().getChatId());
-                ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
-                replyKeyboardMarkup.setSelective(true);
-                replyKeyboardMarkup.setResizeKeyboard(true);
-                replyKeyboardMarkup.setOneTimeKeyboard(false);
-
-                List<KeyboardRow> keyboard = new ArrayList<>();
-                KeyboardRow keyboardFirstRow = new KeyboardRow();
-                keyboardFirstRow.add("Update news");
-                KeyboardRow keyboardSecondRow = new KeyboardRow();
-                keyboardSecondRow.add("settings");
-                keyboardSecondRow.add("rate us");
-                keyboard.add(keyboardFirstRow);
-                keyboard.add(keyboardSecondRow);
-                replyKeyboardMarkup.setKeyboard(keyboard);
-                message.setReplyMarkup(replyKeyboardMarkup);
-
-                try {
-                    // Send the message
-                    execute(message);
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-                /*
-                System.out.println("it has callbackquery");
-                SendMessage mess = new SendMessage()
-                        .setChatId(update.getCallbackQuery().getMessage().getChatId())
-                        .setText(update.getCallbackQuery().getData());
-                try {
-                    execute(mess);
-                }catch (Exception ex){
-                    ex.printStackTrace();
-                }*/
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
             }
         }
-        else {
-            try {
-                    if (update.getMessage().getText().equals("Update news")) {
-                        VKNewsRequest tmp = new VKNewsRequest();
-                        try {
-                            VKNames[] arr = {POLITICS, KINO, NAUKA_I_TECHNICA, CORPORATIONS_I_FIRMS,
-                                    MUSICA, PUTESHESTVIJA, SPORT};
-                            String[] interests = DataBase.getInterest(update.getMessage().getChatId()).split(";");
-                            ArrayList<News> news = new ArrayList<>(0);
-                            System.out.println("size" + interests.length);
-                            for(int i = 0; i < interests.length;i++)
-                                if (interests[i].equals("1"))
-                                    news.addAll(tmp.getVKNews(arr[i], 100, 4));
-                            else
-                                if (interests[i].equals("0"))
-                                    news.addAll(tmp.getVKNews(arr[i], 100, 2));
-                            UpdateNewsToChatId(news, update.getMessage().getChatId());
-                        } catch (Exception ex) {
-                            ex.printStackTrace();
-                        }
-                    } else
-                        if(update.getMessage().getText().equals("settings") || update.getMessage().getText().equals("rate us")){}
-                        else{
-                        try {
-                            if (DataBase.getAllChatId().indexOf(update.getMessage().getChatId()) == -1) {
-                                setOproc(update.getMessage().getChatId(), 0);
-                                DataBase.WriteDB(update);
-                            }
-                        }catch (Exception ex){
-                            ex.printStackTrace();
-                        }
-                    }} catch(Exception ex){
+    }
+
+    /**
+     * Обрабатываем входящее сообщение от пользователя
+     * @param update
+     */
+    void sendNews(Update update){
+        try {
+            if (update.getMessage().getText().equals("Обновить новости")) {
+                VKNewsRequest tmp = usersChoice.get(update.getMessage().getChatId());
+                try {
+                    VKNames[] arr = {POLITICS, KINO, NAUKA_I_TECHNICA, CORPORATIONS_I_FIRMS,
+                            MUSICA, PUTESHESTVIJA, SPORT};
+                    String[] interests = DataBase.getInterest(update.getMessage().getChatId()).split(";");
+                    ArrayList<News> news = new ArrayList<>(0);
+                    System.out.println("size" + interests.length);
+                    for(int i = 0; i < interests.length-1;i++)
+                        if (interests[i].equals("1"))
+                            news.addAll(tmp.getVKNews(arr[i], 100, 4));
+                        else
+                        if (interests[i].equals("0"))
+                            news.addAll(tmp.getVKNews(arr[i], 100, 2));
+                    UpdateNewsToChatId(news, update.getMessage().getChatId());
+                } catch (Exception ex) {
                     ex.printStackTrace();
                 }
+            } else
+            if(update.getMessage().getText().equals("settings") || update.getMessage().getText().equals("rate us")){}
+            else{
+                try {
+                    if (DataBase.getAllChatId().indexOf(update.getMessage().getChatId()) == -1) {
+                        usersChoice.put(update.getMessage().getChatId(), new VKNewsRequest());
+                        setQues(update.getMessage().getChatId(), 0);
+                        DataBase.WriteDB(update);
+                    }
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+            }} catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Метод для обработки входящих запросов к боту
+     * @param update
+     */
+    @Override
+    public void onUpdateReceived(Update update) {
+        if(update.hasCallbackQuery()) {
+            workWithCallbackQuery(update);
+        }
+        else {
+            sendNews(update);
         }
     }
 }
